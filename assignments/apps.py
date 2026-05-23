66158f9a1d934ce5f46206d916d88fabdf8643a5
shashank-mishra123 <kanhapmishra2022@gmail.com>
2026-05-23 15:55:59 +0530

    save current changes

--- assignments/apps.py
+++ assignments/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AssignmentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'assignments'
+# commented out the dashboard view since it's not implemented yet
